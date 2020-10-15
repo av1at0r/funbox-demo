bbb768e51074bcf9a7218be64cef59ab3443476a
Nikita Livyk <livik.nick@gmail.com>
2020-10-15 13:27:34 +0300

    Updates

---
+++ precache-manifest.e64e8c692ef91787dbf0272c78c9c482.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c515878237744cf70c68a6230ef0c13e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b11962082334f868a39",
+    "url": "/static/css/main.69515415.chunk.css"
+  },
+  {
+    "revision": "6d7f8bb284bc1654c2fa",
+    "url": "/static/js/2.8177c2e3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8177c2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b11962082334f868a39",
+    "url": "/static/js/main.4e6b7514.chunk.js"
+  },
+  {
+    "revision": "cac7909737fc14d98ddb",
+    "url": "/static/js/runtime-main.72289c05.js"
+  },
+  {
+    "revision": "79d68011797f7124dd17f3a480a7a01f",
+    "url": "/static/media/bg-pattern.79d68011.png"
+  },
+  {
+    "revision": "c0dcb7d6c9e0087a438ceb982c2ba3c2",
+    "url": "/static/media/cat.c0dcb7d6.png"
+  }
+]);
